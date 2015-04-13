c59650f59d39c31e2dff2844cd21755818333b6f
Laurent (Orbital) <laurent@impermanenceweb.fr>
2015-04-13 09:21:57 +0200

    feat(new): Draft landing page

---
+++ laravel/app/views/home/landing.php
@@ -0,0 +1,395 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Mellifera : Landing page</title>
+	<meta name="description" content="Page de présentation de la solution de gestion de ruches et ruchers">
+	<meta name="keywords" content="abeilles, ruches, ruchers, transhumance">
+	<meta name="author" content="Cyril, Yoann et Laurent">
+	<link rel="apple-touch-icon" sizes="57x57" href="landing/favicons/apple-touch-icon-57x57.png">
+	<link rel="apple-touch-icon" sizes="60x60" href="landing/favicons/apple-touch-icon-60x60.png">
+	<link rel="apple-touch-icon" sizes="72x72" href="landing/favicons/apple-touch-icon-72x72.png">
+	<link rel="apple-touch-icon" sizes="76x76" href="landing/favicons/apple-touch-icon-76x76.png">
+	<link rel="apple-touch-icon" sizes="114x114" href="landing/favicons/apple-touch-icon-114x114.png">
+	<link rel="apple-touch-icon" sizes="120x120" href="landing/favicons/apple-touch-icon-120x120.png">
+	<link rel="apple-touch-icon" sizes="144x144" href="landing/favicons/apple-touch-icon-144x144.png">
+	<link rel="apple-touch-icon" sizes="152x152" href="landing/favicons/apple-touch-icon-152x152.png">
+	<link rel="apple-touch-icon" sizes="180x180" href="landing/favicons/apple-touch-icon-180x180.png">
+	<link rel="icon" type="image/png" href="landing/favicons/favicon-32x32.png" sizes="32x32">
+	<link rel="icon" type="image/png" href="landing/favicons/favicon-194x194.png" sizes="194x194">
+	<link rel="icon" type="image/png" href="landing/favicons/favicon-96x96.png" sizes="96x96">
+	<link rel="icon" type="image/png" href="landing/favicons/android-chrome-192x192.png" sizes="192x192">
+	<link rel="icon" type="image/png" href="landing/favicons/favicon-16x16.png" sizes="16x16">
+	<link rel="manifest" href="landing/favicons/manifest.json">
+	<link rel="shortcut icon" href="landing/favicons/favicon.ico">
+	<meta name="msapplication-TileColor" content="#603cba">
+	<meta name="msapplication-TileImage" content="landing/favicons/mstile-144x144.png">
+	<meta name="msapplication-config" content="landing/favicons/browserconfig.xml">
+	<meta name="theme-color" content="#ffffff">
+	<link rel="stylesheet" href="landing/css/bootstrap.css">
+	<link rel="stylesheet" href="landing/fonts/font-awesome-4.3.0/css/font-awesome.min.css">
+	<link rel="stylesheet" href="landing/css/all.css">
+	<link href='http://fonts.googleapis.com/css?family=Montserrat:400,700|Source+Sans+Pro:400,700,400italic,700italic' rel='stylesheet' type='text/css'>
+</head>
+<body>
+<div id="wrapper">
+		<header id="header">
+			<div class="container">
+				<div class="logo"><a href="#"><img src="landing/images/bee.png" alt="BEE"></a></div>
+				<nav id="nav">
+					<div class="opener-holder">
+						<a href="#" class="nav-opener"><span></span></a>
+					</div>
+					<a href="/backoffice" class="btn btn-primary rounded">Accès au back-office</a>
+					<div class="nav-drop">
+						<ul>
+							<li class="active visible-sm visible-xs"><a href="#">Home</a></li>
+							<li><a href="#">Vue d'ensemble</a></li>
+							<li><a href="#">Code source</a></li>
+							<li><a href="#">License</a></li>
+							<li><a href="#">Support</a></li>
+						</ul>
+						<div class="drop-holder visible-sm visible-xs">
+							<span>Follow Us</span>
+							<ul class="social-networks">
+								<li><a class="fa fa-github" href="#"></a></li>
+								<li><a class="fa fa-twitter" href="#"></a></li>
+								<li><a class="fa fa-facebook" href="#"></a></li>
+							</ul>
+						</div>
+					</div>
+				</nav>
+			</div>
+		</header>
+	<section class="visual">
+		<div class="container">
+			<div class="text-block">
+				<div class="heading-holder">
+					<h1>Bee solutions</h1>
+				</div>
+				<p class="tagline">Un ensemble d'outils de gestion apicole</p>
+				<span class="info">API RestFull, applications mobiles et interface d'administration des données</span>
+			</div>
+		</div>
+		<img src="landing/images/abeille-tournesol.png" alt="" class="bg-stretch">
+	</section>
+	<section class="main">
+		<div class="container">
+			<div id="cta">
+				<a href="#" class="btn btn-primary rounded">Les applications pour smartphones</a>
+				<p>Deux versions Android (SDK et Webview)</p>
+			</div>
+			<div class="row">
+				<div class="text-box col-md-offset-1 col-md-10">
+					<h2>La mobilité en prime</h2>
+					<p>Sur smatphone ou tablette, synchronisez les données collectées sur le terrain, visualisez l'état de vos ruches, organisez vos trashumances</p>
+				</div>
+			</div>
+		</div>
+	</section>
+	<section class="area">
+		<div class="container">
+			<div class="row">
+				<div class="col-md-5">
+					<h2 class="visible-xs visible-sm text-primary">Préparez vos interventions</h2>
+					<ul class="visual-list">
+						<li>
+							<div class="img-holder">
+								<img src="landing/images/graph-04.svg" width="110" alt="">
+							</div>
+							<div class="text-holder">
+								<h3>Gérez vos essaims</h3>
+								<p>Indiquez la race et l'origine d'une reine, son intégration à la colonnie et appréciez la force globale.</p>
+							</div>
+						</li>
+						<li>
+							<div class="img-holder">
+								<img class="pull-left" src="landing/images/graph-03.svg" width="90" alt="">
+							</div>
+							<div class="text-holder">
+								<h3>Listez vos ruches</h3>
+								<p>Visualisez les ruches composant votre rucher, accedez à l'historique de chacune d'entre elles, paramétrez leur caractéristiques.</p>
+							</div>
+						</li>
+						<li>
+							<div class="img-holder">
+								<img src="landing/images/graph-02.svg" height="84" alt="">
+							</div>
+							<div class="text-holder">
+								<h3>Localisez vos ruchers</h3>
+								<p>Un outil de cartographie vous permet de visualiser la disposition de vos ruchers et ainsi vous aider à plannifier les repositionement dans le respect des règles de distance entre rucher.</p>
+							</div>
+						</li>
+						<li>
+							<div class="img-holder">
+								<img src="landing/images/graph-01.svg" height="71" alt="">
+							</div>
+							<div class="text-holder">
+								<h3>Planifiez vos actions</h3>
+								<p>Depuis l'interface de gestion, organisez vos traitements et interventions sur les ruchers, éditez vos étiquettes et feuilles d'intervention. </p>
+							</div>
+						</li>
+					</ul>
+				</div>
+				<div class="col-md-7">
+					<div class="slide-holder">
+						<h2 class="hidden-xs hidden-sm text-primary">Préparez vos interventions</h2>
+						<div class="img-slide scroll-trigger"><img src="landing/images/rucher.png" height="624" width="1184" alt=""></div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</section>
+	<section class="main">
+		<div class="container">
+			<div class="block-holder">
+				<div class="block-frame">
+					<div class="cycle-gallery">
+						<div class="mask">
+							<div class="slideset">
+								<div class="slide">
+									<div class="img-box">
+										<div class="text-holder">
+											<h3>Alane, David et Johnattan</h3>
+										</div>
+									</div>
+									<p>L'équipe en charge des services web et API RestFull, les gardiens des données</p>
+									<em class="date">BeeWebServices</em>
+								</div>
+								<div class="slide">
+									<div class="img-box">
+										<div class="text-holder">
+											<h3>Dominique et Shanti</h3>
+										</div>
+									</div>
+									<p>L'équipe de développeurs de l'application Android version SDK</p>
+									<em class="date">BeeAppli</em>
+								</div>
+								<div class="slide">
+									<div class="img-box">
+										<div class="text-holder">
+											<h3>Hugo et Sébastien</h3>
+										</div>
+									</div>
+									<p>L'autre équipe de développeurs Android, cette fois en version Webview</p>
+									<em class="date">BeeAppWeb</em>
+								</div>
+								<div class="slide">
+									<div class="img-box">
+										<div class="text-holder">
+											<h3>Cyril, Laurent et Yoann</h3>
+										</div>
+									</div>
+									<p>L'équipe en charge de l'interface de gestion</p>
+									<em class="date">BeeBackoffice</em>
+								</div>
+							</div>
+						</div>
+						<a class="btn-prev" href="#"><i class="glyphicon glyphicon-menu-left"></i></a>
+						<a class="btn-next" href="#"><i class="glyphicon glyphicon-menu-right"></i></a>
+					</div>
+				</div>
+			</div>
+		</div>
+	</section>
+	<!-- <section class="visual-container">
+		<div class="visual-area">
+			<div class="container">
+				<h2>People Are Talking About Fork</h2>
+				<ul class="testimonials">
+					<li>
+						<div class="img-holder"><a href="#"><img src=landing/"images/logo-smashing.png" height="43" width="165" alt="smashing magazine"></a></div>
+						<p><em>Sed vestibulum scelerisque urna, eu finibus leo facilisis sit amet. Proin id dignissim magna. Sed varius urna et pulvinar venenatis. </em></p>
+					</li>
+					<li>
+						<div class="img-holder"><a href="#"><img src="landing/images/logo-codrops.png" height="50" width="148" alt="codrops"></a></div>
+						<p><em>Donec euismod dolor ut ultricies consequat. Vivamus urna ipsum, rhoncus molestie neque ac, mollis eleifend nibh.</em></p>
+					</li>
+					<li>
+						<div class="img-holder"><a href="#"><img src="landing/images/logo-w.png" height="64" width="64" alt="w"></a></div>
+						<p><em>In efficitur in velit et tempus. Duis nec odio dapibus, suscipit erat fringilla, imperdiet nibh. Morbi tempus auctor felis ac vehicula. </em></p>
+					</li>
+					<li>
+						<div class="img-holder"><a href="#"><img src="landing/images/logo-pixel.png" height="24" width="225" alt="Pixel Buddha"></a></div>
+						<p><em>Sed vestibulum scelerisque urna, eu finibus leo facilisis sit amet. Proin id dignissim magna. Sed varius urna et pulvinar venenatis. </em></p>
+					</li>
+					<li>
+						<div class="img-holder"><a href=""><img src="landing/images/logo-cb.png" height="34" width="166" alt="creative bloq"></a></div>
+						<p><em>Praesent ut eros tristique, malesuada lectus vel, lobortis massa. Nulla faucibus lorem id arcu consequat faucibus. </em></p>
+					</li>
+					<li>
+						<div class="img-holder"><a href="#"><img src="landing/images/logo-tnw.png" height="34" width="108" alt="tnw"></a></div>
+						<p><em>Fusce pharetra erat id odio blandit, nec pharetra eros venenatis. Pellentesque porttitor cursus massa et vestibulum.</em></p>
+					</li>
+				</ul>
+			</div>
+			<img src="landing/images/img-decor-02.jpg" height="764" width="1380" alt="" class="bg-stretch">
+		</div>
+		<div class="visual-area">
+			<div class="container">
+				<h2>Fork Subscription Pricing</h2>
+				<div class="pricing-tables">
+					<div class="plan">
+						<div class="head">
+							<h3>Students</h3>
+						</div>
+						<div class="price">
+							<span class="price-main"><span class="symbol">$</span>8</span>
+							<span class="price-additional">per month</span>
+						</div>
+						<ul class="item-list">
+							<li>Personal License</li>
+						</ul>
+						<button type="button" class="btn btn-default rounded">purchase</button>
+					</div>
+					<div class="plan">
+						<div class="head">
+							<h3>professional</h3> </div>
+						<div class="price">
+							<span class="price-main"><span class="symbol">$</span>19</span>
+							<span class="price-additional">per month</span>
+						</div>
+							<ul class="item-list">
+							   <li>Professional License</li>
+							   <li>Email Support</li>
+							</ul>
+						<button type="button" class="btn btn-default rounded">purchase</button>
+					</div>
+					<div class="plan recommended">
+						<div class="head">
+							<h3>agency</h3> </div>
+						<div class="price">
+							<span class="price-main"><span class="symbol">$</span>49</span>
+							<span class="price-additional">per month</span>
+						</div>
+							<ul class="item-list">
+								<li>1-12 Team Members</li>
+								<li>Phone Support</li>
+							</ul>
+						<button type="button" class="btn btn-default rounded">purchase</button>
+					</div>
+					<div class="plan">
+						<div class="head">
+							<h3>enterprise</h3> </div>
+						<div class="price">
+							<span class="price-main"><span class="symbol">$</span>79</span>
+							<span class="price-additional">per month</span>
+						</div>
+						<ul class="item-list">
+							<li>Unlimited Team Members</li>
+							<li>24/ 7 Phone Support</li>
+						</ul>
+						<button type="button" class="btn btn-default rounded">purchase</button>
+					</div>
+				</div>
+				<p class="silent">Duis lobortis arcu sed arcu tincidunt feugiat. Nulla nisi mauris, facilisis vitae aliquet id, imperdiet quis nibh. Donec eget elit eu libero tincidunt consequat nec elementum orci. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. </p>
+			</div>
+			<img src="landing/images/img-decor-03.jpg" height="1175" width="1380" alt="" class="bg-stretch">
+		</div>
+	</section>
+	<section class="area">
+		<div class="container">
+			<div class="subscribe">
+				<h3>Subscribe to Our Newsletter</h3>
+				<form class="form-inline">
+					<button type="submit" class="btn btn-primary rounded">Subscribe</button>
+					<div class="form-group">
+						<input type="email" class="form-control rounded" id="exampleInputEmail2" placeholder="Email...">
+					</div>
+				</form>
+			</div>
+		</div>
+	</section>
+	<section class="main">
+		<div class="container">
+			<div class="content">
+				<div class="row">
+					<div class="col-md-4">
+						<h3>New in Release 2.8</h3>
+						<h4>Fully Available: Live Preview Support for Multiple Browsers</h4>
+						<p>Morbi faucibus ante ipsum, a tincidunt libero posuere et. Etiam tempor tortor at odio condimentum, sit amet fringilla ligula maximus. Mauris venenatis nisl nisi, et dictum dui aliquet nec. Sed erat, nec aliquam vel, aliquam ac felis. </p>
+						<h4>Improved Support for Typing </h4>
+						<p>Nulla purus quam, pulvinar sed ante et, feugiat maximus velit. Donec eu elit mauris. </p>
+						<h4>Improved Stability</h4>
+						<p>Proin id ligula eget massa tincidunt molestie. Morbi urna lectus, molestie vel quam vel, iaculis fermentum nunc. Morbi imperdiet, leo in interdum pretium.</p>
+						<div class="btn-holder">
+							<a href="#" class="btn btn-link">Go to Release Log</a>
+						</div>
+					</div>
+					<div class="col-md-4">
+						<h3>Popular Extensions</h3>
+						<h4><a href="#">Custom Themes 1.1</a></h4>
+						<p>Etiam viverra vulputate diam, pulvinar cursus augue egestas ullamcorper. </p>
+						<h4><a href="#">Filter Forge</a></h4>
+						<p>Nulla purus quam, pulvinar sed et, feugiat maximus velit.</p>
+						<h4><a href="#">WebZap</a></h4>
+						<p>Morbi imperdiet, leo in interdum pretium, elit eros dapibus velit, eu posuere quam diam vitae orci. Suspendisse interdum accumsan magna vitae commodo.</p>
+						<h4><a href="#">Renamy</a></h4>
+						<p>Vivamus consectetur suscipit elit, ut lacinia diam elementum et.</p>
+						<div class="btn-holder">
+							<a href="#" class="btn btn-link">Go to Marketplace</a>
+						</div>
+					</div>
+					<div class="col-md-4">
+						<h3>New From the Blog</h3>
+						<h4><a href="#">Morbi imperdiet, leo in interdum pretium.</a></h4>
+						<time class="date" datetime="2015-02-18">February 18, 2015</time>
+						<h4><a href="#">Donec orci ante, porta vel nulla quis, aliquet hendrerit leo.</a></h4>
+						<time class="date" datetime="2015-02-12">February 12, 2015</time>
+						<h4><a href="#">Suspendisse egestas vulputate luctus.</a></h4>
+						<time class="date" datetime="2015-02-06">February 6, 2015</time>
+						<h4><a href="#">Quisque varius ante lorem, eget pretium purus hendrerit a egestas. </a></h4>
+						<time class="date" datetime="2015-02-02">February 2, 2015</time>
+						<h4><a href="#">Phasellus in augue risus. </a></h4>
+						<time class="date" datetime="2015-01-23">January 23, 2015</time>
+						<div class="btn-holder">
+							<a href="#" class="btn btn-link">Go to Blog</a>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</section> -->
+	<footer id="footer">
+		<div class="container">
+			<div class="footer-holder">
+				<div class="row">
+					<div class="col-md-4">
+
+						<p>Lorem ipsum dolor sit amet, feugiat delicat<br /> liberavisse id cum no quo.</p>
+					</div>
+					<div class="col-md-2">
+						<h4>Navigation</h4>
+						<ul>
+							<li><a href="#">Home</a></li>
+							<li><a href="#">Vue d'ensemble</a></li>
+							<li><a href="#">Code source</a></li>
+							<li><a href="#">License</a></li>
+							<li><a href="#">Support</a></li>
+						</ul>
+					</div>
+					<div class="col-md-3">
+						<div class="text-holder">
+							<strong class="phone"><a href="tel:XXXXXXXXXX">XX XX XX XX XX</a></strong>
+							<span class="available">Ne pas déranger</span>
+							<h4>Montpellier, Hérault</h4>
+							<address>1337, boulevard Open Source - 34000</address>
+						</div>
+					</div>
+					<div class="col-md-3">
+						<div class="text-frame">
+							<h4>Info</h4>
+							<p>Wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex commodo consequat. Autem vel hendrerit iriure dolor in hendrerit.</p>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</footer>
+</div>
+<script src="landing/js/jquery-1.11.2.min.js"></script>
+<script src="landing/js/bootstrap.js"></script>
+<script src="landing/js/jquery.main.js"></script>
+</body>
+</html>
