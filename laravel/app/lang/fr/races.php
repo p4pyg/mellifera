4c6135b7dab200cabaefcf20c325f665821f28a4
Lyanthri <cyril.geneys@gmail.com>
2015-04-13 15:51:43 +0200

    fix untracked

---
+++ laravel/app/lang/fr/races.php
@@ -0,0 +1,22 @@
+<?php
+
+return array(
+
+	/*
+	|--------------------------------------------------------------------------
+	| All terms for races
+	|--------------------------------------------------------------------------
+	|
+	| Les lignes suivantes représentent les traductions en français des termes et expressions utilisés lorsque l'on parle de race d'abeille
+	|
+	*/
+
+	"race" 					=> "Race",
+	"races" 				=> "Races",
+	"id" 					=> "Numéro",
+	"raceName"				=> "Nom",
+	"characteristics"		=> "Caracterisques",
+	"geographical_origin"	=> "Origine géographique",
+	"life_span"				=> "Durée de vie",
+	"race_name"				=> "Nom"
+	);
---
+++ laravel/app/views/apiaries/index.blade.php
@@ -0,0 +1,48 @@
+@extends('template')
+@section('content')
+<div class="column-group">
+	<div class="all-100">
+		<h1>@lang('apiaries.apiaries')&nbsp;
+			<small>
+				{{ HTML::decode(
+					HTML::link( 'apiary/edit/', '<span class="fa fa-plus-circle fa-lg"></span>',  trans( 'tools.add' ) ) ) }}
+			</small>
+		</h1>
+		<table id="apiaries" class="ink-table bordered hover alternating" data-page-size="5" data-pagination="#apiariesPagination" >
+			<thead>
+				<tr>
+					<th class="align-left" data-sortable="true">@lang('apiaries.id')</th>
+					<th data-sortable="true">@lang('apiaries.apiary')</th>
+					<th data-sortable="true">@lang('apiaries.life_span')</th>
+					<th data-sortable="true">@lang('apiaries.geographical_origin')</th> 
+					<th data-sortable="true">@lang('apiaries.characteristics')</th>	
+				</tr>
+			</thead>
+			<tbody>
+			@foreach( $apiaries as $apiary )
+				<tr id="apiary-{{ $apiary->id }}" data-item-index="{{ $apiary->id }}">
+					<td>{{ $apiary->id }}</td>
+					<td>{{ $apiary->apiary_name }}</td>
+					<td>{{ $apiary->life_span }}</td>
+					<td>{{ $apiary->geographical_origin }}</td>
+					<td>{{ $apiary->characteristics }}</td>
+				</tr>
+			@endforeach
+			</tbody>
+		</table>
+		<nav class="ink-navigation" id="apiariesPagination">
+			<ul class="pagination black"></ul>
+		</nav>
+		<script>
+			Ink.requireModules( ['Ink.Dom.Selector_1','Ink.UI.Table_1'], function( Selector, Table ){
+				var tableElement = Ink.s('#apiaries');
+				var tableObj = new Table( tableElement );
+			} );
+			$( "tr[id^='apiary']" ).on( 'click', function(){
+				// console.log("apiary_id", $( this ).attr( 'data-item-index' ) );
+				document.location.href="apiary/edit/" + $( this ).attr( 'data-item-index' );
+			} );
+		</script>
+	</div>
+</div>
+@stop
