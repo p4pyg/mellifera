772026de7e20d26a0b3d4e120c889ac543356596
Lyanthri <cyril.geneys@gmail.com>
2015-04-20 21:14:27 +0200

    feat(add):form apiaries

---
+++ laravel/app/views/apiaries/form.blade.php
@@ -0,0 +1,105 @@
+@extends('template')
+@section('content')
+<?php
+if ( is_null( $apiary ) ) {
+	$title 			= trans( 'apiary.create_new_apiary' );
+	$route 			= 'apiary/create';
+	$markup_delete 	= '';
+}else{
+	$title 			= trans( 'apiaries.edit_apiary' );
+	$route 			= 'apiary/update';
+	$markup_delete 	= '<button><span class="fa fa-trash fa-lg"></span></button>';
+}
+?>
+<div class="all-100">
+	<h1>{{ $title }}</h1>
+	<button class="ink-button" id="delete">Delete</button>
+{{	Form::open( [ 'url' => 'apiary/edit/' . $apiary->id, 'method' => 'POST', 'class' => 'ink-form', 'id' => 'apiary_form' ] )	}}
+
+		<div class="column-group gutters">
+			<div class="control-group all-33">
+				<label for="name">@lang( 'apiaries.apiary_name' )</label>
+				<div class="control">
+					<input type="text" name="name" id="name" placeholder="@lang( 'apiaries.apiary_name' )" value="{{ is_null( $apiary ) ? '' : $apiary->apiary_name }}">
+				</div>
+				<p class="tip">Indiquez ici le nom du rucher</p>
+			</div>
+			<div class="control-group all-33">
+				<label for="longitude">@lang( 'apiaries.longitude' )</label>
+				<div class="control">
+					<input type="text" name="longitude" id="longitude" value="{{ is_null( $apiary ) ? '' : $apiary->longitude }}">
+				</div>
+				<p class="tip">Indiquez ici la longitude du rucher</p>
+			</div>
+			<div class="control-group all-33">
+				<label for="latitude">@lang( 'apiaries.latitude' )</label>
+				<div class="control">
+					<input type="text" name="latitude" id="latitude" value="{{ is_null( $apiary ) ? '' : $apiary->latitude }}">
+				</div>
+				<p class="tip">Indiquez ici la latitude du rucher</p>
+			</div>
+			<div class="control-group all-33">
+				<label for="altitude">@lang( 'apiaries.altitude' )</label>
+				<div class="control">
+					<input type="text" name="altitude" id="altitude" value="{{ is_null( $apiary ) ? '' : $apiary->altitude }}">
+				</div>
+				<p class="tip">Indiquez ici l'altitude du rucher</p>
+			</div>
+			<div class="control-group all-33">
+				<label for="vegetation_type">@lang( 'apiaries.vegetation_type' )</label>
+				<div class="control">
+					<input type="text" name="vegetation_type" id="vegetation_type" value="{{ is_null( $apiary ) ? '' : $apiary->vegetation_type }}">
+				</div>
+				<p class="tip">Indiquez ici le type de végétation</p>
+			</div> 
+			<div class="control-group all-33">
+				<label for="hives_capacity">@lang( 'apiaries.hives_capacity' )</label>
+				<div class="control">
+					<input type="text" name="hives_capacity" id="hives_capacity" value="{{ is_null( $apiary ) ? '' : $apiary->hives_capacity }}">
+				</div>
+				<p class="tip">Indiquez ici la capacité de la ruche</p>
+			</div>
+			<div class="control-group all-33">
+				<label for="apiary_notes">@lang( 'apiaries.apiary_notes' )</label>
+				<div class="control">
+					<input type="text" name="apiary_notes" id="apiary_notes" value="{{ is_null( $apiary ) ? '' : $apiary->apiary_notes }}">
+				</div>
+				<p class="tip">Indiquez ici la note du rucher</p>
+			</div>
+			<div class="control-group all-33">
+				<label for="notes">@lang( 'apiaries.notes' )</label>
+				<div class="control">
+					<input type="text" name="notes" id="notes" value="{{ is_null( $apiary ) ? '' : $apiary->notes }}">
+				</div>
+				<p class="tip">Vous pouvez ajouter ici des notes</p>
+			</div> --}}
+		</div>
+		<button class="ink-button" id="valid">Valider</button>
+{{ Form::close() }}
+</div>
+<script>
+// $( '#valid' ).on( 'click', function(){
+// 	var race = $( '#race' ).val();
+// 	var longitude = $( '#longitude' ).val();
+// 	var geographical_origin = $( '#geographical_origin' ).val();
+// 	var clipping = $( '#clipping' ).val();
+// 	var death_date = $( '#death_date' ).val();
+// 	$.post( 'https://bee-mellifera.herokuapp.com/apiary',
+// 		{
+// 			"id": {{ $apiary->id }},
+// 			"transaction":null,
+// 			"unit":null,
+// 			"race": { "id": {{ $apiary->race->id }},"characteristics":null,"geographical_origin": geographical_origin,"life_span":4,"race_name": race },
+// 			"clipping": clipping
+// 		} )
+// 	.done( function( response ){
+// 		console.log( 'response', response );
+// 	} )
+// 	.fail( function( error ){
+// 		console.log( 'error', error );
+// 	} );
+// } );
+			// "longitude": longitude,
+			// "death_date": death_date,
+</script>
+@stop
