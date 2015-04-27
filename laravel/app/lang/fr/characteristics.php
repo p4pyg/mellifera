415ef46ce7218b7bd485416f8b4a0ce4e8dddd4b
Lyanthri <cyril.geneys@gmail.com>
2015-04-27 20:58:14 +0200

    fix: vue hives - delete_old

---
+++ laravel/app/lang/fr/characteristics.php
@@ -0,0 +1,26 @@
+<?php 
+
+return array(
+
+	/*
+	|--------------------------------------------------------------------------
+	| All terms for characteristics
+	|--------------------------------------------------------------------------
+	|
+	| Les lignes suivantes représentent les traductions en français des termes et expressions utilisés lorsque l'on parle de caractéristique
+	|
+	*/
+
+	"characteristics"			=> "Caractéristiques",
+	"id" 						=> "Numéro",
+	"date"						=> "Date",
+	"racial_type"				=> "Race",
+	"aggressivness_level"		=> "Niveau d'agressivité",
+	"swarming_level"			=> "Niveau d'essaim",
+	"winter_hardiness_level"	=> "Niveau de dureté de l'hiver",
+	"wake_up_month"				=> "Mois de réveil"
+
+
+
+
+);
