d6a5e591498a71dae7db4e7846544df40f8a959e
Lyanthri <cyril.geneys@gmail.com>
2015-05-08 14:13:48 +0200

    fix hives lang form & index

--- laravel/app/views/hives/index.blade.php
+++ laravel/app/views/hives/index.blade.php
@@ -4,27 +4,23 @@
 <table id="hives" class="responsive-table hover hoverable striped bordered" data-page-size="5" data-pagination="#hivesPagination" >
 	<thead>
 		<tr>
-			<th class="align-left" data-sortable="true">@lang('hives.id')</th>
-			<th data-sortable="true">@lang('hives.id_lot')</th>
-			<th class="align-center" data-sortable="true">@lang('hives.beehive_type')</th>
-			<th data-sortable="true">@lang('hives.number_of_frames')</th>
-			<th data-sortable="true">@lang('hives.number_of_rocks')</th>
-			<th data-sortable="true">@lang('hives.notes')</th>
-
+			<th class="center-align" data-sortable="true">@lang('hives.id')</th>
+			<th class="center-align" data-sortable="true">@lang('hives.id_lot')</th>
+			<th class="center-align" data-sortable="true">@lang('hives.beehive_type')</th>
+			<th class="center-align" data-sortable="true">@lang('hives.number_of_frames')</th>
+			<th class="center-align" data-sortable="true">@lang('hives.number_of_rocks')</th>
 		</tr>
 	</thead>
 	<tbody>
 	@foreach( $hives as $hive )
 		<tr id="hive-{{ $hive->id }}" data-item-index="{{ $hive->id }}">
-			<td>{{ $hive->id }}</td>
-			<td>{{ $hive->id_lot }}</td>
-			<td>{{ $hive->beehive_type }}</td>
+			<td class="center-align">{{ $hive->id }}</td>
+			<td>{{ ucfirst( $hive->id_lot ) }}</td>
+			<td class="center-align">{{ $hive->beehive_type }}</td>
 			<td>{{ $hive->number_of_frames }}</td>
 			<td>{{ $hive->number_of_rocks }}</td>
-			<td>{{ $hive->notes }}</td>
-		</tr>
 	@endforeach
 	</tbody>
 </table>
-<ul class="pagination" id="hivesPagination"></ul>
+<ul class="pagination"  id="hivesPagination"></ul>
 @stop
