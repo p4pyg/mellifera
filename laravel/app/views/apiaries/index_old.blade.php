6e0c7fcaaff81c7854d0c418429cad88fa5981a4
Lyanthri <cyril.geneys@gmail.com>
2015-05-08 14:14:17 +0200

    fix hives lang form & index

---
+++ laravel/app/views/apiaries/index_old.blade.php
@@ -0,0 +1,46 @@
+@extends('template')
+@section('content')
+@include( 'components.index_header' )
+<table id="apiaries" class="responsive-table hover hoverable striped bordered" data-page-size="5" data-pagination="#apiariesPagination" >
+	<thead>
+		<tr>
+			<th class="align-left" data-sortable="true">@lang( 'apiaries.id' )</th>
+			<th data-sortable="true">@lang( 'apiaries.apiary_name' )</th>
+			<th data-sortable="true">@lang( 'apiaries.person' )</th>
+			<th data-sortable="true">@lang( 'apiaries.address1' )</th>
+			<th data-sortable="true">@lang( 'apiaries.address2' )</th>
+			<th data-sortable="true">@lang( 'apiaries.postcode' )</th>
+			<th data-sortable="true">@lang( 'apiaries.city' )</th>
+			<th data-sortable="true">@lang( 'apiaries.longitude' )</th>
+			<th data-sortable="true">@lang( 'apiaries.latitude' )</th>
+			<th data-sortable="true">@lang( 'apiaries.altitude' )</th>
+			<th data-sortable="true">@lang( 'apiaries.vegetation_type' )</th>
+			<th data-sortable="true">@lang( 'apiaries.hives_capacity' )</th>
+			<th data-sortable="true">@lang( 'apiaries.notes' )</th>
+			<th data-sortable="true">@lang( 'apiaries.rank' )</th>
+		</tr>
+	</thead>
+	<tbody>
+	@foreach( $apiaries as $apiary )
+		<tr id="apiary-{{ $apiary->id }}" data-item-index="{{ $apiary->id }}">
+			<td>{{ $apiary->id }}</td>
+			<td>{{ $apiary->apiary_name }}</td>
+			<td>{{ $apiary->person }}</td>
+			<td>{{ $apiary->address1 }}</td>
+			<td>{{ $apiary->address2 }}</td>
+			<td>{{ $apiary->postcode }}</td>
+			<td>{{ $apiary->city }}</td>
+			<td>{{ $apiary->longitude }}</td>
+			<td>{{ $apiary->latitude }}</td>
+			<td>{{ $apiary->altitude }}</td>
+			<td>{{ $apiary->hives_capacity }}</td>
+			<td>{{ $apiary->vegetation_type }}</td>
+			<td>{{ $apiary->notes }}</td>
+			<td>{{ $apiary->rank }}</td>
+		</tr>
+	@endforeach
+	</tbody>
+</table>
+<ul class="pagination"  id="apiariesPagination"></ul>
+@stop
+
