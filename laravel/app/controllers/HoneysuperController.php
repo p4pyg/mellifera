1ed8ffb6b531a589509a2c0177595c6212906287
Laurent (Orbital) <laurent@impermanenceweb.fr>
2015-06-07 09:09:05 +0200

    fix(): syntax errors

--- laravel/app/controllers/HoneysuperController.php
+++ laravel/app/controllers/HoneysuperController.php
@@ -10,7 +10,10 @@ class HoneysuperController extends \BaseController
     public function index()
     {
         $client 	= new HttpClient;
-        $response 	= $client->get( [ 'url' => Config::get( 'app.api' ) . "atomic/honeysupers", 'headers' 	=> ['Content-type: application/json','APIKEY:' . \Session::get( 'api_token' ) ] ] );
+        $response 	= $client->get( [
+                                    'url' => Config::get( 'app.api' ) . "atomic/honeysupers",
+                                    'headers' 	=> ['Content-type: application/json','APIKEY:' . \Session::get( 'api_token' ) ]
+                                    ] );
         $view 		= BeeTools::is_error( $response );
         if( $view ){
             return $view;
