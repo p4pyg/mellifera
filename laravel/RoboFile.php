86ef89f9df4baf1cfda4177321445f0eb7b0e59c
Laurent (Orbital) <laurent@impermanenceweb.fr>
2015-06-09 14:09:07 +0200

    test():robo

---
+++ laravel/RoboFile.php
@@ -0,0 +1,56 @@
+<?php
+/**
+ * This is project's console commands configuration for Robo task runner.
+ *
+ * @see http://robo.li/
+ */
+class RoboFile extends \Robo\Tasks
+{
+    /**
+     * 	Suppression des fichiers de cache générés lors de la production de vues et la création de session
+     */
+    public function cacheClean(){
+    	$this->taskCleanDir(['../app/storage/sessions','../app/storage/views','../app/storage/debugbar'])->run();
+    }
+
+    /**
+     * 	Composer update
+     */
+    public function packageUpdate(){
+    	$this->taskComposerUpdate()->run();
+    }
+
+    /**
+     *  Git automatic push on multi repo / branch
+     * @param $commit message for the commit
+     * @param $branche branch name destination
+     */
+    public function repoPush( $commit, $branch = null ){
+
+        if ( ! is_null ( $branch ) ){
+            $this->taskGitStack()
+             ->stopOnFail()
+             ->add('-A')
+             ->commit( $commit )
+             ->push('origin', $branch )
+             ->run();
+        }else{
+            $this->taskGitStack()
+             ->stopOnFail()
+             ->add('-A')
+             ->commit( $commit )
+             ->push('origin','master')
+             ->run();
+
+            $this->taskGitStack()
+             ->stopOnFail()
+             ->push('prod', 'master')
+             ->run();
+
+            $this->taskGitStack()
+            ->stopOnFail()
+            ->push('--mirror', 'github')
+            ->run();
+        }
+    }
+}
